1be730a3c87470bd504146adbe08cf27592d02e0
quentin <QuentinL52@users.noreply.huggingface.co>
2026-02-16 21:38:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -97,3 +97,4 @@ if __name__ == "__main__":
     import uvicorn
     port = int(os.getenv("PORT", 8001)) 
     uvicorn.run(app, host="0.0.0.0", port=port)
+CMD ["python", "-m", "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "7860"]
